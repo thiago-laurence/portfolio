c04043799cd9fa8e4fa7fa9017c3bf7e8bac46e9
Thiago Laurence <francolaurence47@gmail.com>
2025-06-12 14:14:04 -0300

    fix: external images

--- src/components/core/utils/AppImage.tsx
+++ src/components/core/utils/AppImage.tsx
@@ -2,5 +2,7 @@ import Image, { ImageProps } from 'next/image'
 import { getBasePath } from '@/lib/utils'
 
 export default function AppImage({ src, alt, ...props }: ImageProps) {
-  return <Image src={getBasePath() + src} alt={alt} {...props} />
+  const isExternal = typeof src === 'string' && src.startsWith('https');
+  const url = (isExternal) ? src : getBasePath() + src;
+  return <Image src={url} alt={alt} {...props} />
 }
